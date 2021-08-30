86099025ec6a2d3b7b633c7619474ea07b2dd4ef
Vulcan-Jack <jacksterdragon@gmail.com>
2021-08-30 17:27:48 -0600

    Creating program

---
+++ JMcarthuBattleship/JMcarthuBattleship/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JMcarthuBattleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
