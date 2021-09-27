ddbc3335ae60ce02bc03e99ef94b336b30c2e811
Jack McArthur <jacksterdragon@gmail.com>
2021-09-27 16:50:04 -0600

    Challenge 3

---
+++ Challenge3/Challenge3/Classroom.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge3
+{
+    class Classroom
+    {
+        public event EventHandler Test;
+
+        public Student bobert = new Student("Bobert T. GoldLewis", "Chemistry", 2.5f, true, 1234567890, 50, 50);
+        public Student joey = new Student("Joey Jilly", "CS", 3.5f, false, 1234567891, 50, 50);
+
+        public Classroom()
+        {
+            bobert.SleepsIn += SleepsIn;
+            Test += bobert.OnTest;
+            Test += bobert.OnTest;
+            Test += joey.OnTest;
+            bobert.SleepIn();
+        }
+
+        public void TestAnnounced()
+        {
+            Console.WriteLine("A test was announced!");
+            Test.Invoke(this, EventArgs.Empty);
+        }
+
+        public void SleepsIn(object sender, EventArgs e)
+        {
+            Console.WriteLine("The classroom is appauled!");
+        }
+
+        public void DropClass(Student student)
+        {
+            Test -= student.OnTest;
+        }
+
+        protected virtual void OnTest(Object sender, EventArgs e)
+        {
+            if(Test != null)
+            {
+                Test.Invoke(sender, e);
+            }
+        }
+    }
+}
---
+++ Challenge3/Challenge3/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge3
+{
+    class Student
+    {
+
+        public event EventHandler SleepsIn;
+
+        private string name, major;
+        private float grade;
+        private bool hangry;
+        private int sid, sadness, energy;
+
+        public Student(string name, string major, float grade, bool hangry, int sid, int sadness, int energy)
+        {
+            this.name = name;
+            this.major = major;
+            this.grade = grade;
+            this.hangry = hangry;
+            this.sid = sid;
+            this.sadness = sadness;
+            this.energy = energy;
+            this.SleepsIn += OnSleepIn;
+            SleepIn();
+        }
+
+        public void OnTest(object sender, EventArgs e)
+        {
+            Console.WriteLine($"{name} starts to panic!");
+        }
+
+        public void SleepIn()
+        {
+            SleepsIn.Invoke(this, EventArgs.Empty);
+        }
+
+        public void OnSleepIn(object sender, EventArgs e)
+        {
+            Console.WriteLine($"{name} has slept in.");
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Major { get => major; set => major = value; }
+        public float Grade { get => grade; set => grade = value; }
+        public bool Hangry { get => hangry; set => hangry = value; }
+        public int Sid { get => sid; set => sid = value; }
+        public int Sadness { get => sadness; set => sadness = value; }
+        public int Energy { get => energy; set => energy = value; }
+    }
+}
